f699b3524d60a91de5137b75534be869386ce342
ashaswi <shresthaashaswi@gmail.com>
2025-02-10 14:37:05 +0545

    added views according to usecase

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,3 +141,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+EMAIL_HOST='127.0.0.1'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_PORT='1025'
