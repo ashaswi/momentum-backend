1aaca05fbf38bc62158e3852037dd38f47e16507
ashaswi <shresthaashaswi@gmail.com>
2025-02-10 14:08:05 +0545

    adding necesarry models according to usecase

--- mysite/models.py
+++ mysite/models.py
@@ -1,6 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from datetime import timedelta
 
-class Task(models.Model):
+class PasswordResetCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def is_expired(self):
+        return now() > self.created_at + timedelta(minutes=15)  # Expire after 15 minutes
+
+class Journal(models.Model):
     title = models.CharField(max_length=255)
-    date = models.DateField()
-    is_completed = models.BooleanField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class MoodEntry(models.Model):
+    content = models.TextField()
+    feeling = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
