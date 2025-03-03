46086fb78d70f5fb304129a3450e06ba84f7ba95
ashaswi <shresthaashaswi@gmail.com>
2025-03-03 13:30:44 +0545

    habit added in model

--- mysite/models.py
+++ mysite/models.py
@@ -54,3 +54,14 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     date = models.DateField()
     is_completed = models.BooleanField()
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    start_date = models.DateField()
+    start_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
