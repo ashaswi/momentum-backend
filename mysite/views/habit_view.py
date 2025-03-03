29890a12fa80c84a21ad5bfbc016f22f31c1653d
ashaswi <shresthaashaswi@gmail.com>
2025-03-03 13:29:37 +0545

    habit view

--- mysite/views/habit_view.py
+++ mysite/views/habit_view.py
@@ -1,16 +1,18 @@
 from rest_framework import viewsets
+from mysite.models import Habit
+from mysite.serializers.habit_serializer import HabitSerializer
 from rest_framework.permissions import IsAuthenticated
-from mysite.models import MoodEntry
-from mysite.serializers.mood_entry_serializer import MoodEntrySerializer
 from rest_framework.filters import OrderingFilter
 
-class MoodEntryViewSet(viewsets.ModelViewSet):
-    queryset = MoodEntry.objects.all()
-    serializer_class = MoodEntrySerializer
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [OrderingFilter]
     ordering_fields = ['id']
     ordering = ['-id']
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user)  # Automatically assign user
--- mysite/views/journal_view.py
+++ mysite/views/journal_view.py
@@ -3,12 +3,16 @@ from rest_framework import viewsets
 from mysite.models import Journal
 from mysite.serializers.journal_serializer import JournalSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import OrderingFilter
 
 
 class JournalViewSet(viewsets.ModelViewSet):
     queryset = Journal.objects.all()
     serializer_class = JournalSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['id']
+    ordering = ['-id']
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
